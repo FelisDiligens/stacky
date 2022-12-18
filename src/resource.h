dfd5e1245390e34b7e5dc634b14c8c03b02bb0bf
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-12-18 11:50:55 +0100

    Updated version to 0.3.3

--- src/resource.h
+++ src/resource.h
@@ -1,6 +1,6 @@
 #define STACKY_ICON_ID		101
 
-#define STACKY_VERSION          0,3,2
-#define STACKY_VERSION_STR      L"0.3.2"
+#define STACKY_VERSION          0,3,3
+#define STACKY_VERSION_STR      L"0.3.3"
 
 #define STACKY_COPYRIGHT_STR    L"Copyright Contributors to the stacky project."
